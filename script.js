3238735cb2f4b6fceb8c352bddf2e722ee8e635e
Aliff Amir <aliff6406@gmail.com@users.noreply.github.com>
2023-04-21 14:04:58 +0800

    add clear function and operate function to operator buttons

--- script.js
+++ script.js
@@ -2,54 +2,86 @@ let num1;
 let num2;
 let operator;
 let displayValue = 0;
+let operatorClicked = false;
 
-const numberBtns = Array.from(document.querySelectorAll('[data-number]'));
+const numberBtnArr = Array.from(document.querySelectorAll('[data-number]'));
+const operatorBtnArr = Array.from(document.querySelectorAll('[data-operator]'));
+const clearBtn = document.getElementById('clear-btn');
+const deleteBtn = document.getElementById('delete-btn');
 const display = document.getElementById('display');
 
-numberBtns.forEach((number) => {
-    number.addEventListener('click', (e) => {
-        concatenateDisplayValue(number.textContent);
+clearBtn.onclick = clearCalculator;
+
+numberBtnArr.forEach((number) => {
+    number.addEventListener('click', () => {
+        concatDisplayValue(number.textContent);
     });
 });
 
-function add(num1, num2) {
+operatorBtnArr.forEach((operatorBtn) => {
+    console.log(operator)
+    operatorBtn.addEventListener('click', () => {
+        if(operatorClicked === false) {
+            num1 = displayValue;
+        } else {
+            num2 = displayValue;
+        }
+        displayValue = 0;
+        dataOperator = operatorBtn.getAttribute('data-operator');
+        
+        if(dataOperator !== '=') {
+            operator = dataOperator;
+        } else {
+            displayValue = operate(operator);
+            console.log(displayValue);
+        }
+        operatorClicked = !operatorClicked;
+    });
+});
+
+function concatDisplayValue(num) {
+    if(displayValue === 0) {
+        displayValue = BigInt(num);
+    } else {
+        displayValue = BigInt(displayValue + '' + num);
+    }
+    updateDisplay();
+};
+
+function clearCalculator() {
+    displayValue = 0;
+    updateDisplay();
+};
+
+function updateDisplay() {
+    display.textContent = displayValue;
+}
+
+function add() {
     return num1 + num2;
 };
 
-function subtract(num1, num2) {
+function subtract() {
     return num1 - num2;
 };
 
-function multiply(num1, num2) {
+function multiply() {
     return num1 * num2;
 };
 
-function divide(num1, num2) {
+function divide() {
     return num1 / num2;
 };
 
-function operate(operator, num1, num2) {
+function operate(operator) {
     switch(operator) {
         case "+":
-            add(num1, num2);
-            break;
+            return add();
         case "-":
-            subtract(num1, num2);
-            break;
+            return subtract();
         case "*":
-            multiply(num1, num2);
-            break;
+            return multiply();
         case "/":
-            divide(num1, num2);
-            break;
+            return divide();
     }
 };
-
-function concatenateDisplayValue(num) {
-    if(displayValue === 0) {
-        displayValue = parseInt(num);
-    } else {
-        displayValue = parseInt(displayValue + '' + num);
-    }
-    display.textContent = displayValue;
-}
